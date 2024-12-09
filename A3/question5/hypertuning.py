1f42f675041cde5a851cf399c1d9923bd7453b96
DanyPaisDaSilva <danypaisdasilva@gmail.com>
2024-12-09 13:31:01 +0100

    new folder structure for question 5

---
+++ A3/question5/hypertuning.py
@@ -0,0 +1,34 @@
+import optuna
+from q5_MLP import objective_MLP
+from q5_RNN import objective_RNN
+from q5_LSTM import objective_LSTM
+
+
+def main():
+    # for MLP
+    study_mlp = optuna.create_study(direction="maximize")
+    study_mlp.optimize(objective_MLP, n_trials=100)
+    print("---------------------- MLP ----------------------")
+    print(f"Best Parameters: {study_mlp.best_params}")
+    print(f"Best Value: {study_mlp.best_value}")
+    print("-------------------------------------------------\n")
+
+    # RNN
+    print("---------------------- RNN ----------------------")
+    study_rnn = optuna.create_study(direction="maximize")
+    study_rnn.optimize(objective_RNN, n_trials=100)
+    print(f"Best Parameters: {study_mlp.best_params}")
+    print(f"Best Value: {study_mlp.best_value}")
+    print("-------------------------------------------------\n")
+
+    # LSTM
+    print("---------------------- LSTM ----------------------")
+    study_lstm = optuna.create_study(direction="maximize")
+    study_lstm.optimize(objective_LSTM, n_trials=100)
+    print(f"Best Parameters: {study_lstm.best_params}")
+    print(f"Best Value: {study_lstm.best_value}")
+    print("--------------------------------------------------\n")
+
+
+if __name__ == "__main__":
+    main()
---
+++ A3/question5/q5_MLP.py
@@ -0,0 +1,71 @@
+from deep_learning_2024.A3 import data_rnn
+import torch
+import torch.optim as optim
+from torch.utils.data import TensorDataset
+from utils import MaxPoolTime, pad, validate
+
+
+class MLPModel(torch.nn.Module):
+    def __init__(self, embedding_size=300, hidden_size=300):
+        super().__init__()
+        numcls = 2
+        hidden = hidden_size
+        embedding_size = embedding_size
+        n_embeddings = 99430  # from len(i2w)
+        self.emb = torch.nn.Embedding(n_embeddings, embedding_size, padding_idx=0)
+        self.fc1 = torch.nn.Linear(embedding_size, hidden)
+        self.fc2 = torch.nn.Linear(hidden, numcls)
+
+    def forward(self, x):
+        x = self.emb(x)
+        x = self.fc1(x)
+        x = torch.nn.functional.relu(x)
+        x = MaxPoolTime(x)
+        x = self.fc2(x)
+
+        return x
+
+
+def objective_MLP(trial):
+    embedding_size = trial.suggest_int("embedding_size", 50, 500, step=50)
+    hidden_size = trial.suggest_int("hidden_size", 50, 500, step=50)
+    learning_rate = trial.suggest_float("lr", 1e-5, 1e-1, log=True)
+    batch_size = trial.suggest_categorical("batch_size", [128, 256, 512, 1024])
+    nr_epochs = trial.suggest_categorical("nr_epochs", [10, 20, 30])
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # load data
+    (x_train, y_train), (x_val, y_val), (i2w, w2i), numcls = data_rnn.load_imdb(final=False)
+    padded_train = torch.stack([pad(x) for x in x_train])
+    padded_val = torch.stack([pad(x) for x in x_val])
+
+    padded_train = padded_train.to(device)
+    padded_val = padded_val.to(device)
+
+    train_dataset = TensorDataset(padded_train, torch.tensor(y_train))
+    validation_dataset = TensorDataset(padded_val, torch.tensor(y_val))
+    trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    valloader = torch.utils.data.DataLoader(validation_dataset, batch_size=batch_size, shuffle=False)
+
+    model = MLPModel(embedding_size, hidden_size)
+    model.train()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    for epoch in range(nr_epochs):
+
+        for i, batch in enumerate(trainloader, 0):
+            instances, labels = batch
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = model(instances)
+            loss = torch.nn.functional.cross_entropy(outputs, labels)
+            loss.backward()
+            optimizer.step()
+    # run on validation set
+    val_acc = validate(model, valloader)
+
+    return val_acc
